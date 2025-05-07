d397a8969b39a092eb7a0b1868a73020d624a659
Mario Costa De Souza <mariocos@c1r9s9.42lisboa.com>
2025-05-07 17:51:30 +0100

    adding to work on lap

--- Post_Dele/Post_master.hpp
+++ Post_Dele/Post_master.hpp
@@ -1,6 +1,7 @@
 #ifndef POST_MASTER_HPP
 #define POST_MASTER_HPP
 
+#include "../includes/webserv.hpp"
 
 class Post_master
 {
@@ -9,8 +10,21 @@ private:
 	
 
 public:
+
+	static void post(Client* client);
 	Post_master(/* args */);
 	~Post_master();
+
+	class EvilRequest : public std::exception
+	{
+		public:
+			const char	*what(void)const throw(); 
+	};
+	class BadClientPointer : public std::exception
+	{
+		public:
+			const char	*what(void)const throw(); 
+	};
 };
 
 Post_master::Post_master(/* args */)
@@ -21,8 +35,21 @@ Post_master::~Post_master()
 {
 }
 
+const char *Post_master::EvilRequest::what() const  throw()
+{
+	return("Evil_request\n");
+}
+const char *Post_master::BadClientPointer::what() const  throw()
+{
+	return("BadClientPointer\n");
+}
 
-
+void	Post_master::post(Client* client)
+{
+	if (!client)
+		throw (BadClientPointer());
+	client->getClientRequest()->get_path();
+}
 
 
 
